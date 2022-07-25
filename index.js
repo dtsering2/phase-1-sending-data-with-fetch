8fcbe686cb06556cd5e489e1952f627b76bd015e
Tsering Tsering <dtsering2@gmail.com>
2022-07-25 15:10:27 -0400

    commit tree bf4e3b997f35c37c79fd50af184add98dc787599

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your code here
+
+function submitData(someName,someEmail){
+    return fetch("http://localhost:3000/users",{
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name : someName,
+            email: someEmail,
+        })
+    })
+    .then(res =>res.json())
+    .then(resObj => appendDom(resObj))
+    .catch(function appendDomErr(err){
+   
+        p.textContent = 'Unauthorized Access'
+        body.append(p)
+    })
+}
+let body = document.querySelector('body');
+const p = document.createElement('p')
+
+function appendDom(someObj){
+    p.textContent = someObj.id
+    body.append(p)
+}
+
+
+
+
+
+
+
